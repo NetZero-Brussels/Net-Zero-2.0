86c09212497569d475df395a4366e1bc5ec1f647
ferrabled <frabascol@gmail.com>
2024-08-07 21:42:48 +0200

    Capacitor & Ionic install and setup

--- net-zero-2.0/next.config.mjs
+++ net-zero-2.0/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "",
+    transpilePackages: ["@ionic/react", "@ionic/core", "ionicons"],
+    output: "export",
+};
 
 export default nextConfig;
