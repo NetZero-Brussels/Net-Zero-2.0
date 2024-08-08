ca17f9c7cc9660f5761533d1c120257cd2f22260
ferrabled <frabascol@gmail.com>
2024-08-08 18:29:35 +0200

    Nextjs project reset: Delete net-zero-2.0 folder
    Unable to use next js with capacitor due to the routing both frameworks use

--- net-zero-2.0/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.netzero.app',
-  appName: 'net-zero-2.0',
-  webDir: 'out'
-};
-
-export default config;
--- net-zero-2.0/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "",
-    transpilePackages: ["@ionic/react", "@ionic/core", "ionicons"],
-    output: "export",
-};
-
-export default nextConfig;
