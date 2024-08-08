17962f568d6ec6ce41cd783eb0a92eda4fb40448
ferrabled <frabascol@gmail.com>
2024-08-08 19:04:24 +0200

    Frontend set up: Capacitor & Ionic init

--- net-zero-2.0/src/App.tsx
+++ net-zero-2.0/src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonRouterOutlet,
+  setupIonicReact,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './variables.css';
+
+setupIonicReact({});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <IonApp>
+      
+      <IonReactRouter>
+        <IonRouterOutlet id="main">
+          <Route path="/landing" render={() => <></>} />
+          <Route path="/register" render={() => <></>} />
+
+          <Route path="/tabs" render={() => <></>} />
+          <Route path="/dashboard" render={() => <></>} />
+          <Route
+            path="/"
+            render={() => <Redirect to="/landing" />}
+            exact={true}
+          />
+        </IonRouterOutlet>
+      </IonReactRouter>
+      
+    </IonApp>
   );
 }
 
