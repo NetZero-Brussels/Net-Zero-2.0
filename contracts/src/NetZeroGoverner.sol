84df1b0747b64153660178c750446acc79b514a3
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-08-08 01:49:59 +0100

    update-governer-with-vote-allocation-attestation-verifier

--- contracts/src/NetZeroGoverner.sol
+++ contracts/src/NetZeroGoverner.sol
@@ -6,10 +6,62 @@ import {GovernorTimelockControl} from "../lib/openzeppelin-contracts/contracts/g
 import {TimelockController} from "../lib/openzeppelin-contracts/contracts/governance/TimelockController.sol";
 
 contract NetZeroGoverner is Governor, GovernorTimelockControl {
+
+    struct project {
+        uint64 votes;
+        uint64 projectId;
+    }
+
+    struct VoteAllocation {
+        bytes voterId;
+        uint256 numVotes;
+        uint256 timestamp;
+    }
+    mapping(bytes => VoteAllocation) private allocations;
+    mapping(uint256 => uint256) private voteIDToAllocation;
+    mapping(uint256 => bytes) private voteIdToNullifierHash;
+    address public resolver;
+    uint256 private totalVoteCount;
+
     constructor(TimelockController _timelock)
         Governor("Net Zero Governer")
         GovernorTimelockControl(_timelock)
-    {}
+    {
+        resolver = _resolver;
+        totalVoteCount = 0;
+    }
+
+
+    function allocateVotes(bytes32[] memory voterHashes, uint256[] memory numVotes, bytes32 message, bytes memory signature) public {
+        require(verifyAttestation(message, signature), "Invalid attestation");
+        require(voterHashes.length == numVotes.length, "Invalid number of votes compared to voters");
+
+        for (uint i = 0; i < voterHashes.length; i++) {
+            require(allocations[voterHashes[i]].timestamp == 0, "Votes already allocated");
+
+            allocations[voterHashes[i]] = VoteAllocation({
+                voterHash: voterHashes[i],
+                numVotes: numVotes[i],
+                timestamp: block.timestamp
+            });
+
+            totalVoteCount += numVotes[i];
+        }
+    }
+
+    function verifyAttestation(bytes32 message, bytes memory signature) internal view returns (bool) {
+        (bool success, bytes memory result) = resolver.staticcall(abi.encodeWithSignature("verifyAttestation(bytes32,bytes)", message, signature));
+        require(success, "Verification failed");
+        return abi.decode(result, (bool));
+    }
+
+    function getTotalVotes() public view returns (uint256) {
+        return totalVoteCount;
+    }
+
+    function getVotes(bytes32 voterHash) public view returns (uint256) {
+        return allocations[voterHash].numVotes;
+    }
 
     function votingDelay() public pure virtual override returns (uint256) {
         return 1 days;
