ab13476773b4136ac30751615fcf8c7e8ea5fbfd
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-08-10 11:18:20 +0100

    addl

--- contracts/script/deploy.sh
+++ contracts/script/deploy.sh
@@ -1,3 +1,3 @@
 
 source ../.env
-forge script script/DeployNetZeroGovernor.s.sol --rpc-url $PRIVATE_KEY --broadcast --private-key $PRIVATE_KEY
+forge script script/NetZeroGovener.s.sol:NetZeroGovenerScript --rpc-url $RPC_URL --etherscan-api-key $TENDERLY_ACCESS_KEY --verify --verifier-url https://virtual.base-sepolia.rpc.tenderly.co/106b4376-0051-4ff8-b2cd-8f5b11b99537/verify/etherscan --broadcast --private-key b8f2fd311196bb910f43acedc56cd2e9aeb6b2b61f4922e258cfcc8ded296513
