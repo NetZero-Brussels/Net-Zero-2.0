fd08ee31a34be4bf6f4302d80bb40a62d182dad5
ddharvey <58657636+ddharvey@users.noreply.github.com>
2024-08-10 23:49:10 +0200

    fa

--- farcaster/farcaster-frame/farcaster-frame/api/index.tsx
+++ farcaster/farcaster-frame/farcaster-frame/api/index.tsx
@@ -154,7 +154,6 @@ app.frame('/', async (c) => {
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined';
 const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development';
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
 
 export const GET = handle(app);
 export const POST = handle(app);
