7182958cd4fa22d500b8db892b32255c603f72f8
ferrabled <frabascol@gmail.com>
2024-08-11 12:43:22 +0200

    Simplify Farcaster Frame

--- farcaster/farcaster-frame/farcaster-frame/api/index.tsx
+++ farcaster/farcaster-frame/farcaster-frame/api/index.tsx
@@ -2,7 +2,6 @@ import { Button, Frog, TextInput } from "frog";
 import { devtools } from "frog/dev";
 import { serveStatic } from "frog/serve-static";
 import { handle } from "frog/vercel";
-import { ethers } from "ethers";
 import abi from "./abi.json";
 
 export const app = new Frog({
@@ -11,77 +10,9 @@ export const app = new Frog({
   title: "Reputation Leaderboard",
 });
 
-const mockLeaderboard = [
-  { address: "0x123...", name: "Alice", reputation: 150 },
-  { address: "0x456...", name: "Bob", reputation: 120 },
-  { address: "0x789...", name: "Charlie", reputation: 100 },
-];
-
-const contractAddress = "0x4e7A32FAd4364710A81e6B98B64cdc14C5a9E29D";
-
-async function getContractInstance() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-  await provider.send("eth_requestAccounts", []);
-
-  const signer = provider.getSigner();
-
-  const contract = new ethers.Contract(contractAddress, contractABI, signer);
-  return contract;
-}
-
-async function getUserWalletAddress() {
-  try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    const walletAddress = await signer.getAddress();
-    console.log(`User's wallet address: ${walletAddress}`);
-    return walletAddress;
-  } catch (error) {
-    console.error("Failed to get wallet address:", error);
-    return null;
-  }
-}
-
-function formatPosition(position) {
-  const index = parseInt(position);
-  const suffixes = ["th", "st", "nd", "rd"];
-  const emoji = ["🥇", "🥈", "🥉"];
-
-  const suffix =
-    suffixes[index % 10 > 3 || Math.floor(index / 10) === 1 ? 0 : index % 10];
-  const emojiSymbol = emoji[index - 1] || "";
-
-  return `${index}${suffix}`.trim();
-}
-
-function formatEmoji(position) {
-  const index = parseInt(position);
-  const suffixes = ["th", "st", "nd", "rd"];
-  const emoji = ["🥇", "🥈", "🥉"];
-
-  const suffix =
-    suffixes[index % 10 > 3 || Math.floor(index / 10) === 1 ? 0 : index % 10];
-  const emojiSymbol = emoji[index - 1] || "";
-
-  return `${emojiSymbol}`.trim();
-}
-
-async function fetchLeaderboard() {
-  try {
-    return mockLeaderboard;
-  } catch (error) {
-    console.error("Failed to fetch leaderboard:", error);
-    return [];
-  }
-}
-
 app.frame("/", async (c) => {
   const { buttonValue, inputText, status } = c;
   const position = inputText || buttonValue;
-
-  const leaderboard = await fetchLeaderboard();
 
   return c.res({
     image: (
@@ -114,19 +45,7 @@ app.frame("/", async (c) => {
             whiteSpace: "pre-wrap",
           }}
         >
-          {status === "response"
-            ? position
-              ? `${formatEmoji(position)} The user in ${formatPosition(
-                  position
-                )} is ${
-                  leaderboard[position - 1]?.name || "Unknown User"
-                } with address ${
-                  leaderboard[position - 1]?.address || "N/A"
-                } and a reputation score of ${
-                  leaderboard[position - 1]?.reputation || 0
-                } points.`
-              : ""
-            : "🌱 Net Zero Reputation Leaderboard 📋"}
+          {"🌱 Net Zero 2.0 📋"}
         </div>
 
         <div
@@ -140,32 +59,7 @@ app.frame("/", async (c) => {
             marginTop: 20,
           }}
         >
-          {leaderboard.length > 0 ? (
-            <ul
-              style={{
-                listStyleType: "none",
-                padding: 0,
-                display: "flex",
-                flexDirection: "row",
-                gap: 10,
-              }}
-            >
-              {leaderboard.map((user, index) => (
-                <li
-                  key={user.address}
-                  style={{
-                    marginBottom: 10,
-                    padding: "5px 0",
-                  }}
-                >
-                  {index + 1}. {user.name || user.address}: {user.reputation}{" "}
-                  points
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <div>No users found or unable to fetch data.</div>
-          )}
+          {"I am already a voter in NetZero."}
         </div>
 
         <div
@@ -177,13 +71,17 @@ app.frame("/", async (c) => {
             fontSize: 20,
             marginTop: 40,
           }}
-        ></div>
+        >
+          {"Let's save the planet together!"}
+        </div>
       </div>
     ),
     intents: [
       <TextInput placeholder="Choose your position..." />,
-      <Button value="1">1st 🥇</Button>,
-      <Button.Transaction target="/send-tx">Create acc.</Button.Transaction>,
+      <Button.Link href="https://net-zero-2-0.vercel.app/">
+        Visit NetZero
+      </Button.Link>,
+      <Button.Transaction target="/send-tx">Create account</Button.Transaction>,
       status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
   });
