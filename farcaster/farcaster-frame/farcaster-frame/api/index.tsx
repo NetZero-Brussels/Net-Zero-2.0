1cfbac6d6839c76c7aef848ab849535a196432bf
ferrabled <frabascol@gmail.com>
2024-08-11 13:26:54 +0200

    Farcaster Minor fix

--- farcaster/farcaster-frame/farcaster-frame/api/index.tsx
+++ farcaster/farcaster-frame/farcaster-frame/api/index.tsx
@@ -8,53 +8,54 @@ export const app = new Frog({
   title: "Net Zero 2.0",
 });
 
-app.frame("/", async (c) => {
+app.frame("/", (c) => {
   return c.res({
-    action: "/finish",
     image: (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          background: "linear-gradient(to right, #432889, #17101F)",
-          backgroundSize: "100% 100%",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            fontSize: 60,
-            fontStyle: "normal",
-            letterSpacing: "-0.025em",
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: "0 120px",
-            whiteSpace: "pre-wrap",
-          }}
-        >
-          {"🌱 Net Zero 2.0 📋"}
-        </div>
-
+      <div>
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
             alignItems: "center",
-            color: "white",
-            fontSize: 38,
-            fontWeight: "bold",
-            marginTop: 20,
+            background: "linear-gradient(to right, #432889, #17101F)",
+            backgroundSize: "100% 100%",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
           }}
         >
-          <div>{"I am already a voter in NetZero."}</div>
-          <div>{"What are you waiting for?"}</div>
-          <div>{"Let's save the planet together!"}</div>
+          <div
+            style={{
+              color: "white",
+              fontSize: 60,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: "0 120px",
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {"🌱 Net Zero 2.0 📋"}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              color: "white",
+              fontSize: 38,
+              fontWeight: "bold",
+              marginTop: 20,
+            }}
+          >
+            <div>{"I am already a voter in NetZero."}</div>
+            <div>{"What are you waiting for?"}</div>
+            <div>{"Let's save the planet together!"}</div>
+          </div>
         </div>
       </div>
     ),
