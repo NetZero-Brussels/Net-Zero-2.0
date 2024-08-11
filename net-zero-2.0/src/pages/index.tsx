27fd093a0c733f5f9a1888c5531974a0554216fb
ferrabled <frabascol@gmail.com>
2024-08-11 08:15:12 +0200

    Institution Routing and Tabs

--- net-zero-2.0/src/pages/index.tsx
+++ net-zero-2.0/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import LandingPage from "./landing";
 import { Onboarding, OnboardingCompleted } from "./onboarding";
 import { VoterTabs } from "./voter";
+import { ITabs } from "./institution";
 
-export { LandingPage, Onboarding, OnboardingCompleted, VoterTabs };
+export { LandingPage, Onboarding, OnboardingCompleted, VoterTabs, ITabs };
---
+++ net-zero-2.0/src/pages/institution/ITabs.tsx
@@ -0,0 +1,60 @@
+import { Redirect, Route } from "react-router-dom";
+import {
+  IonRouterOutlet,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonPage,
+} from "@ionic/react";
+import {
+  cog,
+  flash,
+  list,
+  personCircleOutline,
+  personOutline,
+  rocketOutline,
+} from "ionicons/icons";
+import DiscoveryPage from "./DiscoveryPage";
+
+import { Header } from "../../components";
+import IProfilePage from "./ProfilePage";
+
+const ITabs = () => {
+  return (
+    <IonPage>
+      <IonTabs>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/institutions/discovery">
+            <IonIcon icon={rocketOutline} />
+            <IonLabel>Discovery</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/institutions/account">
+            <IonIcon icon={personOutline} />
+            <IonLabel>Account</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+        <IonRouterOutlet>
+          <Route
+            path="/institutions/discovery"
+            render={() => <DiscoveryPage />}
+            exact={true}
+          />
+          <Route
+            path="/institutions/account"
+            render={() => <IProfilePage />}
+            exact={true}
+          />
+          <Route
+            path="/institutions"
+            render={() => <Redirect to="/institutions/discovery" />}
+            exact={true}
+          />
+        </IonRouterOutlet>
+      </IonTabs>
+    </IonPage>
+  );
+};
+
+export default ITabs;
